5fe8bdc9446b8d955b8ce1e8b1e2cd40e97304ed
schauss <schauss@dmanz.org>
2025-03-24 14:18:23 -0700

    (buggy) naive division

--- test/BigInt.cxx
+++ test/BigInt.cxx
@@ -215,7 +215,109 @@ TEST_CASE("operator: * multiplication") {
   }
 }
 
+TEST_CASE("division 1") {
+  sch::BigInt dividend{"46072918281146839449"};
+  sch::BigInt divisor{3};
+  std::cout << dividend / divisor;
+}
+
+TEST_CASE("division") {
+  for (int i = 0; i < 100; ++i) {
+    sch::BigInt bint[2];
+    sch::BigInt10 n[2];
+    std::ostringstream os[2];
+
+    for (int k = 0; k < 2; ++k) {
+      std::string str = randomString(1, 20);
+      // randomizeSign(str);
+      n[k] = str;
+      bint[k] = str;
+    }
+    if (n[1] == 0) {
+      n[1] = randomInRange(1, 9);
+      bint[1] = n[1].to_string();
+    }
+
+    os[0] << n[0] / n[1];
+    os[1] << bint[0] / bint[1];
+    CHECK(os[0].str() == os[1].str());
+    if (os[0].str() != os[1].str()) {
+      std::cout << n[0] << " / " << n[1] << " ..... " << bint[0] << " / "
+                << bint[1] << '\n';
+    }
+  }
+}
+
+/*
+
+TEST_CASE("operator: % modulo") {
+  for (int i = 0; i < 1000; ++i) {
+    long long n[2];
+    sch::BigInt bint[2];
+    std::ostringstream os[2];
+
+    for (int k = 0; k < 2; ++k) {
+      std::string str = randomString(1, LL_FULL_LENGTH);
+      randomizeSign(str);
+      n[k] = std::stoll(str);
+      if (n[k] == 0) {
+        n[k] = randomInRange(1, 9); // NOLINT
+      }
+      bint[k] = n[k];
+    }
+    os[0] << n[0] % n[1];
+    os[1] << bint[0] % bint[1];
+    CHECK(os[0].str() == os[1].str());
+  }
+}
+
+// TODO consider sign
+TEST_CASE("sch::pow()") {
+  SKIP();
+  for (int i = 0; i < 11; ++i) {
+    for (int j = 0; j < 11; ++j) {
+      sch::BigInt a{i};
+      std::ostringstream os1;
+      std::ostringstream os2;
+      os1 << static_cast<unsigned long long>(std::pow(i, j));
+      // os2 << sch::pow(a, j);
+      CHECK(os1.str() == os2.str());
+    }
+  }
+}
+
+TEST_CASE("counting to 1 million") {
+  // todo fix sign change on increment
+  sch::BigInt n{0};
+  for (size_t i = 0; i < 1E6; ++i) { // NOLINT
+    ++n;
+  }
+  CHECK(n == 1000000);
+  n = -1000001;
+  for (size_t i = 0; i < 1E6; ++i) { // NOLINT
+    ++n;
+  }
+  CHECK(n == sch::BigInt{-1});
+}
+
+TEST_CASE("counting from 1 million") {
+  // todo fix sign change on decrement
+  sch::BigInt n{1000000};
+  for (size_t i = 1E6; i > 0; --i) {
+    --n;
+  }
+  CHECK(n == sch::BigInt{0});
+  n = -1;
+  for (size_t i = 1E6; i > 0; --i) {
+    --n;
+  }
+  CHECK(n == -1000001);
+}
+
+*/
+
 // todo special cases -- 1's 0's etc.
+// todo truncate fractional part of doubles
 TEST_CASE("templated operators") {
   SECTION("std::string") {
     for (int i = 0; i < 1000; ++i) {
@@ -561,94 +663,5 @@ TEST_CASE("templated operators") {
     }
   }
 }
-
-/*
-
-TEST_CASE("operator: / division") {
-  for (int i = 0; i < 1000; ++i) {
-    long long n[2];
-    sch::BigInt bint[2];
-    std::ostringstream os[2];
-
-    for (int k = 0; k < 2; ++k) {
-      std::string str = randomString(1, LL_FULL_LENGTH);
-      randomizeSign(str);
-      n[k] = std::stoll(str);
-      if (n[k] == 0) {
-        n[k] = randomInRange(1, 9); // NOLINT
-      }
-      bint[k] = n[k];
-    }
-    os[0] << n[0] / n[1];
-    os[1] << bint[0] / bint[1];
-    CHECK(os[0].str() == os[1].str());
-  }
-}
-
-TEST_CASE("operator: % modulo") {
-  for (int i = 0; i < 1000; ++i) {
-    long long n[2];
-    sch::BigInt bint[2];
-    std::ostringstream os[2];
-
-    for (int k = 0; k < 2; ++k) {
-      std::string str = randomString(1, LL_FULL_LENGTH);
-      randomizeSign(str);
-      n[k] = std::stoll(str);
-      if (n[k] == 0) {
-        n[k] = randomInRange(1, 9); // NOLINT
-      }
-      bint[k] = n[k];
-    }
-    os[0] << n[0] % n[1];
-    os[1] << bint[0] % bint[1];
-    CHECK(os[0].str() == os[1].str());
-  }
-}
-
-// TODO consider sign
-TEST_CASE("sch::pow()") {
-  SKIP();
-  for (int i = 0; i < 11; ++i) {
-    for (int j = 0; j < 11; ++j) {
-      sch::BigInt a{i};
-      std::ostringstream os1;
-      std::ostringstream os2;
-      os1 << static_cast<unsigned long long>(std::pow(i, j));
-      // os2 << sch::pow(a, j);
-      CHECK(os1.str() == os2.str());
-    }
-  }
-}
-
-TEST_CASE("counting to 1 million") {
-  // todo fix sign change on increment
-  sch::BigInt n{0};
-  for (size_t i = 0; i < 1E6; ++i) { // NOLINT
-    ++n;
-  }
-  CHECK(n == 1000000);
-  n = -1000001;
-  for (size_t i = 0; i < 1E6; ++i) { // NOLINT
-    ++n;
-  }
-  CHECK(n == sch::BigInt{-1});
-}
-
-TEST_CASE("counting from 1 million") {
-  // todo fix sign change on decrement
-  sch::BigInt n{1000000};
-  for (size_t i = 1E6; i > 0; --i) {
-    --n;
-  }
-  CHECK(n == sch::BigInt{0});
-  n = -1;
-  for (size_t i = 1E6; i > 0; --i) {
-    --n;
-  }
-  CHECK(n == -1000001);
-}
-
-*/
 
 } // namespace BigInt_test
