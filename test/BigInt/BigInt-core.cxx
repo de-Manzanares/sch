c4418c0c12138f4c374c0192c4a715b95af42d12
schauss <schauss@dmanz.org>
2025-05-14 11:47:16 -0700

    BigInt operators /= % %=

--- test/BigInt/BigInt-core.cxx
+++ test/BigInt/BigInt-core.cxx
@@ -146,23 +146,29 @@ TEST_CASE("division") {
 }
 
 TEST_CASE("modulo") {
-  for (int i = 0; i < 1000; ++i) {
-    long long n[2];
+  for (int i = 0; i < 50; ++i) {
     sch::BigInt bint[2];
+    sch::BigInt10 n[2];
     std::ostringstream os[2];
 
     for (int k = 0; k < 2; ++k) {
-      std::string str = random_string(1, LL_FULL_LENGTH);
+      std::string str = random_string(1, 2000);
       randomize_sign(str);
-      n[k] = std::stoll(str);
-      if (n[k] == 0) {
-        n[k] = random_in_range(1, 9); // NOLINT
-      }
-      bint[k] = n[k];
+      n[k] = str;
+      bint[k] = str;
     }
+    if (n[1] == 0) {
+      n[1] = random_in_range(1, 9);
+      bint[1] = n[1].to_string();
+    }
+
     os[0] << n[0] % n[1];
     os[1] << bint[0] % bint[1];
     CHECK(os[0].str() == os[1].str());
+    if (os[0].str() != os[1].str()) {
+      std::cout << n[0] << " % " << n[1] << " ..... " << bint[0] << " % "
+                << bint[1] << '\n';
+    }
   }
 }
 
@@ -182,35 +188,6 @@ TEST_CASE("exponentiation") {
     }
   }
 }
-
-TEST_CASE("counting to 1 million") {
-  // todo fix Sign change on increment
-  sch::BigInt n{0};
-  for (size_t i = 0; i < 1E6; ++i) { // NOLINT
-    ++n;
-  }
-  CHECK(n == 1000000);
-  n = -1000001;
-  for (size_t i = 0; i < 1E6; ++i) { // NOLINT
-    ++n;
-  }
-  CHECK(n == sch::BigInt{-1});
-}
-
-TEST_CASE("counting from 1 million") {
-  // todo fix Sign change on decrement
-  sch::BigInt n{1000000};
-  for (size_t i = 1E6; i > 0; --i) {
-    --n;
-  }
-  CHECK(n == sch::BigInt{0});
-  n = -1;
-  for (size_t i = 1E6; i > 0; --i) {
-    --n;
-  }
-  CHECK(n == -1000001);
-}
-
 */
 
 } // namespace big_int_test
